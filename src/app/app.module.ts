b4197f1c68c729c6804dffdb5e842a526d1bb658
edcorona <lev.corona@gmail.com>
2023-10-31 17:01:08 -0600

    ngFor list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroesModule } from './heroes/heroes.module';
+
+//import { MainPageComponent } from './dbz/pages/main-page.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    //MainPageComponent
   ],
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
